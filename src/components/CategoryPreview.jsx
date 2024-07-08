04f1b3da49ec29903c40ba8e618f9e60dc67688b
piecanoe <karenkaydu@gmail.com>
2024-07-08 00:07:08 -0700

    Change "callout" to "category"

--- src/components/CategoryPreview.jsx
+++ src/components/CategoryPreview.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const callouts = [
+const categories = [
   {
-    name: 'Desk and Office',
-    description: 'Work from home accessories',
+    name: 'Bolo Ties',
+    description: 'Dress up time',
     imageSrc:
       'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
     imageAlt:
@@ -11,8 +11,8 @@ const callouts = [
     href: '#',
   },
   {
-    name: 'Self-Improvement',
-    description: 'Journals and note-taking',
+    name: 'Tops',
+    description: 'Spice up your jacket',
     imageSrc:
       'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
     imageAlt:
@@ -20,8 +20,8 @@ const callouts = [
     href: '#',
   },
   {
-    name: 'Travel',
-    description: 'Daily commute essentials',
+    name: 'Bottoms',
+    description: 'Gotta have pants',
     imageSrc:
       'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
     imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
@@ -37,23 +37,23 @@ export default function CategoryPreview() {
           <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-            {callouts.map((callout) => (
-              <div key={callout.name} className="group relative">
+            {categories.map((category) => (
+              <div key={category.name} className="group relative">
                 <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                   <img
-                    alt={callout.imageAlt}
-                    src={callout.imageSrc}
+                    alt={category.imageAlt}
+                    src={category.imageSrc}
                     className="h-full w-full object-cover object-center"
                   />
                 </div>
                 <h3 className="mt-6 text-sm text-gray-500">
-                  <a href={callout.href}>
+                  <a href={category.href}>
                     <span className="absolute inset-0" />
-                    {callout.name}
+                    {category.name}
                   </a>
                 </h3>
                 <p className="text-base font-semibold text-gray-900">
-                  {callout.description}
+                  {category.description}
                 </p>
               </div>
             ))}
