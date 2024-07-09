cbd12f1ae9e4d2a5129946bf2b917ae41adacae9
piecanoe <karenkaydu@gmail.com>
2024-07-09 00:11:33 -0700

    Add 3rd object with category of "bolo ties", change out image urls

--- src/products.js
+++ src/products.js
@@ -66,7 +66,61 @@ const products = [
     ],
     images: [
       {
-        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
+        src: 'https://media.kohlsimg.com/is/image/kohls/3023504_Olive_Green?wid=620&hei=620&op_sharpen=1',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
+        alt: 'Model wearing plain black basic tee.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
+        alt: 'Model wearing plain gray basic tee.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
+        alt: 'Model wearing plain white basic tee.',
+      },
+    ],
+    colors: [
+      { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+      { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+      { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    ],
+    sizes: [
+      { name: 'XXS', inStock: false },
+      { name: 'XS', inStock: true },
+      { name: 'S', inStock: true },
+      { name: 'M', inStock: true },
+      { name: 'L', inStock: true },
+      { name: 'XL', inStock: true },
+      { name: '2XL', inStock: true },
+      { name: '3XL', inStock: true },
+    ],
+    description:
+      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+      'Hand cut and sewn locally',
+      'Dyed with our proprietary colors',
+      'Pre-washed & pre-shrunk',
+      'Ultra-soft 100% cotton',
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  },
+  {
+    id: '3',
+    name: 'Product 3',
+    price: '$192',
+    href: '#',
+    category: 'bolo ties',
+    breadcrumbs: [
+      { id: 1, name: 'Men', href: '#' },
+      { id: 2, name: 'Clothing', href: '#' },
+    ],
+    images: [
+      {
+        src: 'https://kalifano.com/cdn/shop/products/kalifano-native-american-jewelry-lewistus-silversmith-navajo-lapis-flower-usa-native-american-made-925-sterling-silver-bolo-tie-nabt2200-001-36311902060738.jpg?v=1675151353',
         alt: 'Two each of gray, white, and black shirts laying flat.',
       },
       {
