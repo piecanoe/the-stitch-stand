7b43624d880be9cc94cea76bf54b748a82715698
piecanoe <karenkaydu@gmail.com>
2024-07-10 23:27:49 -0700

    Create events.js (currently unused)

---
+++ src/events.js
@@ -0,0 +1,22 @@
+const events = [
+  {
+    id: '1',
+    name: 'The Frogtown Flea Crawl',
+    date: 'Saturday, October 28, 2024',
+    img: 'https://marianainla.com/wp-content/uploads/2023/10/unnamed-3-5-833x1024.jpg',
+  },
+  {
+    id: '2',
+    name: 'Anime Expo',
+    date: 'Friday, July 3, 2024',
+    img: 'https://www.unionarena-tcg.com/na/images/events/anime_expo/2024/img_mv_sp.jpg?v3',
+  },
+  {
+    id: '3',
+    name: "Echo Park Farmer's Market",
+    date: 'Sunday, May 5, 2024',
+    img: 'https://cdn2.allevents.in/thumbs/thumb665ca29b471bd.jpg',
+  },
+];
+
+export default events;
