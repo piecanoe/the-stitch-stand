ff502daf3aebd078dc8d41ad7381ed38dec089e4
piecanoe <karenkaydu@gmail.com>
2024-09-27 13:33:03 -0700

    Add multer and awsRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,20 @@ import cors from 'cors';
 import eventRoutes from './eventRoutes.js';
 import productRoutes from './productRoutes.js';
 import userRoutes from './userRoutes.js';
+import awsRoutes from './awsRoutes.js';
+import multer from 'multer';
+const upload = multer();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(upload.any());
 app.use(eventRoutes);
 app.use(productRoutes);
 app.use(userRoutes);
+app.use(awsRoutes);
 
 app.listen(PORT, () => {
   connectToServer();
