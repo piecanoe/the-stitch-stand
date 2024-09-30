d0b2378c8632baf106f7e8c63a6581ce7033eaa1
piecanoe <karenkaydu@gmail.com>
2024-09-30 09:23:00 -0700

    Fix image property

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export const EventCard = ({ event }) => {
-  let date = new Date(event.date);
-  let stringDate = date.toString();
+  // let date = new Date(event.date);
+  // let stringDate = date.toString();
 
   return (
     <div className="m-4 p-4 bg-blue-300 hover:bg-red-300 cursor-pointer rounded-md">
-      <img src={event.img} />
+      <img src={event.imageId} />
       <h1>{event.name}</h1>
-      <h2>{stringDate.slice(4, 15)}</h2>
+      {/* <h2>{stringDate.slice(4, 15)}</h2> */}
+      <h2>{event.date}</h2>
     </div>
   );
 };
